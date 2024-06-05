76687dac21e497b4ce26094dcac949da597d8926
yayo1983 <yazanenator@gmail.com>
2024-06-05 10:55:42 -0600

    somre example for use mlfloow client for create client or get experiments

---
+++ mlflow_client.py
@@ -0,0 +1,17 @@
+import mlflow
+from mlflow import MlflowClient
+from mlflow.entities import ViewType
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+client = MlflowClient()
+
+experiments = client.search_experiments(view_type=ViewType.ALL,
+                                        filter_string="tags.`version` = 'v1' AND tags.`framework` = 'sklearn'",
+                                        order_by=["experiment_id ASC"]
+                                        )
+
+for exp in experiments:
+    print(f"Experiment Name: {exp.name}, Experiment ID: {exp.experiment_id}")
+    
+    
---
+++ mlflow_create_exp_client.py
@@ -0,0 +1,24 @@
+
+import mlflow
+from mlflow import MlflowClient
+from mlflow.entities import ViewType
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+client = MlflowClient()
+
+run = client.create_run(
+    experiment_id="1",
+    tags={
+        "Version": "v1",
+        "Priority": "P1"
+    },
+    run_name="run from client 2"
+)
+
+print(f"Run tags: {run.data.tags}")
+print(f"Experiment id: {run.info.experiment_id}")
+print(f"Run id: {run.info.run_id}")
+print(f"Run name: {run.info.run_name}")
+print(f"lifecycle_stage: {run.info.lifecycle_stage}")
+print(f"status: {run.info.status}")
